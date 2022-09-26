ee83d4a05c24aed5cbe5547720ba33dd78664591
anniraz <zarinakudajberdikyzy@gmail.com>
2022-09-26 20:19:30 +0600

    third commit

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,14 +1,15 @@
-# !/bin/sh
+#!/bin/sh
 
 if [ "$DATABASE" = "postgres" ]
-then 
-    echo "Waiting for postgres"
+then
+    echo "Waiting for postgres..."
 
     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
         sleep 0.1
     done
 
-    echo "Postgresql started"
-
+    echo "PostgreSQL started"
 fi
+
 exec "$@"
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$DATABASE" = "postgres" ]
 then 
     echo "Waiting for postgres"
-     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
         sleep 0.1
     done
 
@@ -11,7 +11,5 @@ then
 
 fi
 
-python manage.py flush --no-input
-python manage.py migrate
 
 exec "$@"
